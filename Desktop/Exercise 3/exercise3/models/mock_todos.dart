24a8db4d94dcc1f86cd44d7a68f866487755238f
Lawandhassan <lhqu180102@uniq.edu.iq>
2021-11-23 01:18:24 +0300

    This is exercise 3 dirver is lawand

---
+++ Desktop/Exercise 3/exercise3/models/mock_todos.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'todo.dart';
+import 'task.dart';
+
+final myTodoList = <Todo>[
+
+Todo(title: "MAP Project", task: [
+
+  Task{title: 'Deliverable #1: Project Proposal and Design', completed: false},
+  Task{title: 'Deliverable #2: Prototype 1', completed: false},
+  Task{title: 'Deliverable #3: Prototype 2', completed: false},
+  Task{title: 'Deliverable #4: Backend', completed: true},
+  Task{title: 'Deliverable #5: Release Version', completed: false},
+
+]),
+
+Todo(title: "Prepering for Online learning", task: [
+
+  Task{title: 'Deliverable #1: Project Proposal and Design', completed: false},
+  Task{title: 'Deliverable #2: Prototype 1', completed: false},
+  Task{title: 'Deliverable #3: Prototype 2', completed: false},
+  Task{title: 'Deliverable #4: Backend', completed: true},
+  Task{title: 'Deliverable #5: Release Version', completed: false},
+
+]),
+
+Todo(title: "MAP Project", task: [
+
+  Task{title: 'Deliverable #1: Project Proposal and Design', completed: false},
+  Task{title: 'Deliverable #2: Prototype 1', completed: false},
+  Task{title: 'Deliverable #3: Prototype 2', completed: false},
+  Task{title: 'Deliverable #4: Backend', completed: true},
+  Task{title: 'Deliverable #5: Release Version', completed: false},
+
+]),
+
+
+
+
+
+
+
+
+];
